af36ac8239504d8303958ed0f8c199b6d028c0b4
kimura <pankata79.th@gmail.com>
2020-11-15 07:53:35 +0900

    リポジトリ層実装

--- src/main/java/com/example/demo/repository/MenuRepository.java
+++ src/main/java/com/example/demo/repository/MenuRepository.java
@@ -1,5 +1,33 @@
 package com.example.demo.repository;
 
-public class MenuRepository {
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.example.demo.domain.Menu;
+
+
+
+@Mapper
+public interface MenuRepository {
+	
+	@Select("select * from menu")
+	List<Menu> selectAll();
+	
+	@Select("select * from menu where id=#{id}")
+	Menu selectPK(int id);
+	
+	@Insert("insert into menu values(#{id},#{title},#{cource},#{recette},#{memo})")
+	void insert(Menu menu);
+	
+	@Update("update menu set title=#{title},cource=#{cource},recette=#{recette},memo=#{memo},where id=#{id}")
+	void update(Menu menu);
+	
+	@Delete("delete from menu where id=#{id}")
+	void delete(int id);
 
 }
