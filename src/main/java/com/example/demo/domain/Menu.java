98d2b5922db6a096e338baf2b0bc3ed83cdf33c4
kimura <pankata79.th@gmail.com>
2020-11-15 07:54:18 +0900

    ドメイン

--- src/main/java/com/example/demo/domain/Menu.java
+++ src/main/java/com/example/demo/domain/Menu.java
@@ -1,5 +1,66 @@
 package com.example.demo.domain;
 
+import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class Menu {
+	
+	@Id
+	private int id;
+	
+	@NotBlank
+	@Size(min = 1,max = 50)
+	private String title;
+	
+	@Max(value = 10)
+	private String course;
+	
+	@Max(value = 1000)
+	private String recette;
+	
+	@Max(value = 1000)
+	private String memo;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getRecette() {
+		return recette;
+	}
+
+	public void setRecette(String recette) {
+		this.recette = recette;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
 
 }
